c8db847cdf637b7ab13f513d62f7c00dbe85d42b
Pol Curto <pol.curto@gmail.com>
2025-01-29 16:42:53 +0100

    Boss basic behaviour

--- Assets/Scripts/Boss/BossLifeComponent.cs
+++ Assets/Scripts/Boss/BossLifeComponent.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class BossLifeComponent : LifeComponent
 {
     [SerializeField] private BossScript bossScript;
+
+    public override void GetHit(int damage)
+    {
+        base.GetHit(damage);
+
+        if (currentLife == 1)
+        {
+            bossScript.HalfLife();
+        }
+    }
+
     override protected void Die()
     {
         bossScript.Die();
